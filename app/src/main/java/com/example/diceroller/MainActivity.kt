16f62712a26ea683b16e2a7e786684f9735f81f6
Tracywangui <Tracy.ndegwa@strathmore.edu>
2023-10-12 18:51:43 +0300

    finished DiceRoller

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+
+        // Do a dice roll when the app starts
+        rollDice()
+    }
+}
+class Dice(val numSides : Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
+private fun rollDice() {
+    val dice = Dice(6)
+    val diceRoll = dice.roll()
+    val drawableResource = when (diceRoll) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    diceImage.setImageResource(drawableResource)
+}
